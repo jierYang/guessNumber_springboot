ce3d621f23be320cfc404c093697a3ad0892095d
Yang Jie <jiyang@thoughtworks.com>
2018-09-17 16:20:59 +0800

    init springboot project

---
+++ src/main/java/com/tw/train/guessNumber_springboot/GuessNumberSpringbootApplication.java
@@ -0,0 +1,12 @@
+package com.tw.train.guessNumber_springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuessNumberSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuessNumberSpringbootApplication.class, args);
+	}
+}
